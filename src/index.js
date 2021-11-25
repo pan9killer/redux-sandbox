ce5210be7bf0af3f66f719e0239a67d89a10a1c1
pan9killer <pan9killer@gmail.com>
2021-11-25 12:36:58 +0400

    redux first UI

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const reducer = (state = 0, action) => {
   switch (action.type){
     case 'INC':
       return state + 1;
+    
+    case 'DEC':
+      return state -1;
 
     default:
       return state; 
@@ -12,9 +15,11 @@ const reducer = (state = 0, action) => {
 
 const store = createStore(reducer);
 
-store.subscribe(()=>{
-  console.log(store.getState());
-});
+document.getElementById('inc').addEventListener('click', ()=>{store.dispatch({type: 'INC'})});
+document.getElementById('dec').addEventListener('click', ()=>{store.dispatch({type: 'DEC'})});
 
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
+const update = () => {
+  document.getElementById('counter').innerHTML = store.getState();
+}
+
+store.subscribe(update)
