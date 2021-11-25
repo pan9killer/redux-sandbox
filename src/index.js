e366f73b74ab6450bf279983930d5de9429b2510
pan9killer <pan9killer@gmail.com>
2021-11-25 13:00:02 +0400

    file refactor

--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
 import { createStore } from "redux";
-
-const reducer = (state = 0, action) => {
-  switch (action.type){
-    case 'RND':
-      return state + action.payload;
-
-    case 'INC':
-      return state + 1;
-    
-    case 'DEC':
-      return state -1;
-
-    default:
-      return state; 
-  }
-}
+import { inc, dec, rnd } from './actions';
+import reducer from "./reducer";
 
 const store = createStore(reducer);
-
-const inc = () => ({type: 'INC'});
-const dec = () => ({type: 'DEC'});
-const rnd = (payload) => ({type: 'RND', payload});
 
 document.getElementById('inc').addEventListener('click', ()=>{store.dispatch(inc())});
 document.getElementById('dec').addEventListener('click', ()=>{store.dispatch(dec())});
