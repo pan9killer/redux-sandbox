63ca70baf22d207c950b4a91fea151aac446c211
pan9killer <pan9killer@gmail.com>
2021-11-25 12:48:54 +0400

    redux reducer add params case

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import { createStore } from "redux";
 
 const reducer = (state = 0, action) => {
   switch (action.type){
+    case 'RND':
+      return state + action.payload;
+
     case 'INC':
       return state + 1;
     
@@ -17,6 +20,9 @@ const store = createStore(reducer);
 
 document.getElementById('inc').addEventListener('click', ()=>{store.dispatch({type: 'INC'})});
 document.getElementById('dec').addEventListener('click', ()=>{store.dispatch({type: 'DEC'})});
+document.getElementById('rnd').addEventListener('click', ()=>{
+  const payload = Math.floor(Math.random()*11);
+  store.dispatch({type: 'RND', payload})});
 
 const update = () => {
   document.getElementById('counter').innerHTML = store.getState();
