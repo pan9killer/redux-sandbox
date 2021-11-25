47f86ffafdcd7371b9b7aaa88a2a54e2574648ae
pan9killer <pan9killer@gmail.com>
2021-11-25 12:54:19 +0400

    add action creators

--- src/index.js
+++ src/index.js
@@ -18,11 +18,15 @@ const reducer = (state = 0, action) => {
 
 const store = createStore(reducer);
 
-document.getElementById('inc').addEventListener('click', ()=>{store.dispatch({type: 'INC'})});
-document.getElementById('dec').addEventListener('click', ()=>{store.dispatch({type: 'DEC'})});
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const rnd = (payload) => ({type: 'RND', payload});
+
+document.getElementById('inc').addEventListener('click', ()=>{store.dispatch(inc())});
+document.getElementById('dec').addEventListener('click', ()=>{store.dispatch(dec())});
 document.getElementById('rnd').addEventListener('click', ()=>{
   const payload = Math.floor(Math.random()*11);
-  store.dispatch({type: 'RND', payload})});
+  store.dispatch(rnd(payload))});
 
 const update = () => {
   document.getElementById('counter').innerHTML = store.getState();
